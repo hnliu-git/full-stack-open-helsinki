63f451a762f30ff0531da2b542e62a69729d685a
Haonan Liu <liu@textkernel.nl>
2022-11-16 08:02:46 +0100

    ex 1.1-1.2

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,43 @@
+const Header = (props) => (
+  <>
+    <h1>
+      {props.course}
+    </h1>
+  </>
+)
+
+const Part = (props) => (
+  <><p>{props.part} {props.exe}</p></>
+)
+
+const Content = (props) => (
+  <>
+    <Part part={props.part1} exe={props.exercises1} />
+    <Part part={props.part2} exe={props.exercises2} /> 
+    <Part part={props.part3} exe={props.exercises3} />
+  </>
+)
+
+const Total = (props) => (
+  <><p>Number of exercises {props.total}</p></>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
